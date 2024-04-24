4fe1b8cd19ad236da7e935703487855b63274b55
Dmytro B. <dmbaikalov@gmail.com>
2024-04-24 13:41:28 +0300

    TC 01.1.1_10

---
+++ tests/redirectToHomePage.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+
+
+test.describe('Redirection from different pages into Home page', () => {
+    const PAGES = {
+        "whatsNew'": "/what-is-new.html",
+        "women": "/women.html",
+        "men": "/men.thml",
+        "gear": "gear.html",
+        "training": "/training.html",
+        "sale": "/sale.html"
+    }
+
+    const HOMEPAGE_LINK = "https://magento.softwaretestingboard.com/"
+
+    test.beforeEach(async ({page}) => {
+        await page.goto('/');
+        const LOGO_LOCATOR = page.locator('.logo');
+    })
+
+    test('Redirection from Training page to Home page', async ({page}) => {
+        await page.goto(PAGES.training);
+        await page.locator('.logo').click();
+        await expect(page).toHaveURL(HOMEPAGE_LINK);
+
+    })
+})
